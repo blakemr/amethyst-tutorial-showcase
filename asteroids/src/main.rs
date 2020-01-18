01d3ba31109959c6dcc175cc4f8258fb2849ce72
Matthew Blake <mblak0625@gmail.com>
2020-01-18 10:42:00 -0500

    asteroids - added window

--- asteroids/src/main.rs
+++ asteroids/src/main.rs
@@ -30,15 +30,26 @@ fn main() -> amethyst::Result<()> {
     // Set up the game window
     // By setting it up to load the config from a file, you don't have to
     // recompile every time you change the graphics settings.
-    // see: config/display.ron
+    // also sets up root directory path, which will be used to reference
+    // all the other files.
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
 
     // Basic application setup
-    let game_data = GameDataBuilder::default();
+    let game_data = GameDataBuilder::default().with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin sets up everything you need to open a window and draw on it.
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            // RenderFlat2D plugin is used to render entities with a 'SpriteRender' component.
+            .with_plugin(RenderFlat2D::default()),
+    )?;
 
     let assets_dir = app_root.join("assets");
     let mut world = World::new();
+
     let mut game = Application::new(assets_dir, BaseState, game_data)?;
 
     // This starts the game loop. The game will continue until a state returns
