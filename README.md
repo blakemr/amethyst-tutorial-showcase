d76925537fab01834116bcf71d8bcd6a4cb6374d
Matthew Blake <mblak0625@gmail.com>
2020-01-18 09:59:58 -0500

    Set up basic game state for asteroids.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 * Snake - snake clone
 * Flappy Pig - Flappy Bird clone but I like the "when pigs fly" joke
 * Prairie King - Clone of that arcade game from Stardew Valley
+
+These projects are based on Amethyst version 0.13
--- asteroids/src/main.rs
+++ asteroids/src/main.rs
@@ -1,6 +1,4 @@
 use amethyst::{
-    core::transform::TransformBundle,
-    ecs::prelude::{ReadExpect, Resources, SystemData},
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
@@ -10,32 +8,41 @@ use amethyst::{
     utils::application_root_dir,
 };
 
-struct MyState;
+// Initial game state.
+// The tutorial names this the same as the game name,
+// but I don't like that since working titles are common.
+pub struct BaseState;
 
-impl SimpleState for MyState {
-    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
-}
+// Simpletate is a simplified State trait
+// SimpleState: https://docs-src.amethyst.rs/stable/amethyst/prelude/trait.SimpleState.html
+// State: https://docs-src.amethyst.rs/stable/amethyst/prelude/trait.State.html
+// "State" in the book: https://book.amethyst.rs/stable/concepts/state.html
+//
+// To clarify, State uses generics for all its implementation, whereas
+// SimpleState fills in these generics with what you would commonly use.
+impl SimpleState for BaseState {}
 
 fn main() -> amethyst::Result<()> {
+    // Set up a logger for game testing/debugging
+    // This spits out all your logging messages into the terminal.
     amethyst::start_logger(Default::default());
 
+    // Set up the game window
+    // By setting it up to load the config from a file, you don't have to
+    // recompile every time you change the graphics settings.
+    // see: config/display.ron
     let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
 
-    let config_dir = app_root.join("config");
-    let display_config_path = config_dir.join("display.ron");
+    // Basic application setup
+    let game_data = GameDataBuilder::default();
 
-    let game_data = GameDataBuilder::default()
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)
-                        .with_clear([0.34, 0.36, 0.52, 1.0]),
-                )
-                .with_plugin(RenderFlat2D::default()),
-        )?
-        .with_bundle(TransformBundle::new())?;
+    let assets_dir = app_root.join("assets");
+    let mut world = World::new();
+    let mut game = Application::new(assets_dir, BaseState, game_data)?;
 
-    let mut game = Application::new("/", MyState, game_data)?;
+    // This starts the game loop. The game will continue until a state returns
+    // Trans::Quit or all states are popped off the stack.
     game.run();
 
     Ok(())
